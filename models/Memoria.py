4961d70a50e898fd3a060eda541b161e1fb8dca7
edlgg <delagarzaguerra@gmail.com>
2018-11-29 10:43:58 -0600

    wip

--- models/Memoria.py
+++ models/Memoria.py
@@ -1,3 +1,46 @@
 class Memoria:
+  def __init__(self, paginas):
+    self.tabla = []
+    for i in range(paginas):
+      self.tabla.append(Row(1, 1, 1))
   
+  def __repr__(self):
+    return "[{}]".format("".join(self.tabla))
+        
+  def __str__(self):
+    return "[{}]".format("-".join(self.tabla))
+
+  def hasRow(self, newRow):
+    for row in self.tabla:
+      if newRow.pid == row.pid and newRow.pagina == row.pagina:
+        return True
+    return False
+  
+  def insertar(self,newRow):
+    return
+  
+  def eliminar(self, newRow):
+    return
+
+  def lru(self, newRow):
+    return None
+
+  def actualizarTiempo(newRow):
+    return
+
+  
+class Row:
+  def __init__(self, pid, pagina, tiempo):
+    self.pid = pid
+    self.pagina = pagina
+    self.tiempo = tiempo
+
+  def __repr__(self):
+        return "(%d, %d, %d)\n" % (self.pid, self.pagina, self.tiempo)
+
+  def __str__(self):
+      return "(%d, %d, %d)" % (self.pid, self.pagina, self.tiempo)
+
+  # def __str__(self):
+  #   return "pid: " + str(self.pid) + " pagina: " + str(self.pagina)
   
---
+++ models/testMemoria.py
@@ -0,0 +1,5 @@
+from Memoria import Memoria
+from Memoria import Row
+t = Memoria(3)
+row = Row(2,1,1)
+print(t.hasRow(row))
