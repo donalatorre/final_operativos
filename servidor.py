7bb9736a0f0a980d675f0a148bebb4cd1de31533
edlgg <delagarzaguerra@gmail.com>
2018-11-29 12:30:31 -0600

    wip

--- servidor.py
+++ servidor.py
@@ -1,5 +1,13 @@
 import socket
 
+from Central import Central
+# from Cpu import Cpu
+# from Impresora import Impresora
+from Memoria import Memoria
+from MemoriaAlloc import MemoriaAlloc
+from Proceso import Proceso
+
+
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('localhost', 8088))
 serversocket.listen(1)
@@ -7,10 +15,13 @@ while True:
     connection, address = serversocket.accept()
     buf = connection.recv(64)
     if buf != 'END':
-        print buf
+        handleMessage(buf)
     else:
       serversocket.close()
       connection.close()
       break
+
+def handleMessage(buf):
+
 
         
