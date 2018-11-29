a8d5587981ad427bb214e1ba29555700213af85c
synthesisnapo <napoleon.lazo@hotmail.com>
2018-11-29 12:11:57 -0600

    Cpu actualiza tiempos en procesos

--- models/Cpu.py
+++ models/Cpu.py
@@ -1,4 +1,5 @@
 import proceso
+import datetime
 
 class Cpu:
 
@@ -8,19 +9,36 @@ class Cpu:
 		self.quantumval = quantum
 		self.colaListos = []
 		self.listaTerminados = []
+		self.tLlegadaCpu = 0
+		self.tTotalCpu = 0
+
 
 	##proceso es un objeto tipo proceso
 	def anadirProceso(self, proceso):
 		self.colaListos.append(proceso)
+		if(self.colaListos[0] == proceso):
+			self.tLlegadaCpu = datetime.datetime.now().time()
 
 	def Quantum(self):
-		aux = self.colaListos.pop(0);
+		
+		self.tTotalCpu = (datetime.datetime.combine(datetime.datetime.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.datetime.today(), self.tLlegadaCpu)).total_seconds()
+		self.tTotalCpu = round(self.tTotalCpu, 4)
+
+		print(self.tTotalCpu)
+		aux = self.colaListos.pop(0)
+		aux.tCPU += self.tTotalCpu
+		self.tLlegadaCpu = datetime.datetime.now().time()
 		self.colaListos.append(aux)
 
 	##pid es un string
 	def terminarProceso(self, pid):
 		for x in self.colaListos:
 			if(x.pid == pid):
+				if(self.colaListos[0] == x):
+					self.tTotalCpu = (datetime.datetime.combine(datetime.datetime.today(), datetime.datetime.now().time()) - datetime.datetime.combine(datetime.datetime.today(), self.tLlegadaCpu)).total_seconds()
+					self.tTotalCpu = round(self.tTotalCpu, 4)
+					x.tCPU += self.tTotalCpu
+
 				self.listaTerminados.append(x)
 				self.colaListos.remove(x)
 
