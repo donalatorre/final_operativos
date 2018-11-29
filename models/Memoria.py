2d508a18bf04b4881dcecf3f78eed70b270cc403
edlgg <delagarzaguerra@gmail.com>
2018-11-29 11:08:03 -0600

    memoria

--- models/Memoria.py
+++ models/Memoria.py
@@ -1,8 +1,9 @@
 class Memoria:
   def __init__(self, paginas):
     self.tabla = []
+    self.limite = paginas
     for i in range(paginas):
-      self.tabla.append(Row(1, 1, 1))
+      self.tabla.append(Row(-1, -1, -1))
   
   def __repr__(self):
     return "[{}]".format("".join(self.tabla))
@@ -17,16 +18,37 @@ class Memoria:
     return False
   
   def insertar(self,newRow):
-    return
+    for i in range(self.limite -1):
+      if self.tabla[i].pid == -1:
+        self.tabla[i] = newRow
+        return True
+    return False
+
   
   def eliminar(self, newRow):
-    return
+    for i, row in enumerate(self.tabla):
+      if newRow.pid == row.pid and newRow.pagina == row.pagina:
+        self.tabla[i] = Row(-1,-1,-1)
+        return True
+    return False
 
   def lru(self, newRow):
-    return None
+    minIndex = 0
+    minTiempo = self.tabla[0].tiempo
+    for i, row in enumerate(self.table):
+      if row.tiempo < minTiempo:
+        minIndex = i
+        minTiempo = row.tiempo
+    oldRow = self.tabla[i]
+    self.tabla[i] = newRow
+    return oldRow
 
-  def actualizarTiempo(newRow):
-    return
+  def actualizarTiempo(self, newRow):
+    for i, row in enumerate(self.tabla):
+      if newRow.pid == row.pid and newRow.pagina == row.pagina:
+        self.tabla[i] = newRow
+        return True
+    return False
 
   
 class Row:
--- models/testMemoria.py
+++ models/testMemoria.py
@@ -2,4 +2,12 @@ from Memoria import Memoria
 from Memoria import Row
 t = Memoria(3)
 row = Row(2,1,1)
-print(t.hasRow(row))
+print(t.tabla)
+r = t.insertar(row)
+print(r)
+print(t.tabla)
+
+row = Row(1,1,3)
+r = t.actualizarTiempo(row)
+print(r)
+print(t.tabla)
